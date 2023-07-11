9877c87b5378acc3558b88faf6f8ebe20545b4c8
Mohan Subudhi <111859729+Spupun@users.noreply.github.com>
2023-07-11 22:36:00 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,27 +1,45 @@
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login form</title>
-
-   
+    <title>Login Page</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 
 <body>
-
     <form action="login_action.php" method="post">
-        <b>Username:</b><input type="text" name="email" placeholder="enter your name" required><br>
-        <b>Password:</b><input type="password" name="password" placeholder="enter your password" required><br>
-        <input type="submit" name="submit" value="submit"><br>
-        <span>Don't have accout ?</span>
-        <span><a href="signup.php">Sign Up here</a></span>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 mt-5">
+                    <h2 class="text-center">Login Form</h2>
+                    <form>
+                        <div class="form-group">
+                            <label for="username">Username</label>
+                            <input type="text" class="form-control" name="email" id="username" placeholder="Enter username">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" name="password" id="password" placeholder="Enter password">
+                        </div>
+                        <button type="submit" name="submit" class="btn btn-primary">Login</button>
+                    </form>
+                </div>
+            </div>
+            <div class="row justify-content-center mt-3">
+                <div class="col-md-6">
+                    <p class="text-center">Don't have an account? <a href="signup.php">Sign up</a></p>
+                </div>
+            </div>
+        </div>
     </form>
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
-
-
-
-<?php
